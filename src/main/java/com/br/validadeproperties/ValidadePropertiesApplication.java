1e9797a3023911dbbfb519170cdd3f88d7369b84
lucashsmoraes <lucashsmoraes@gmail.com>
2024-06-28 13:58:56 -0300

    Teste para validar actions e template checklist

---
+++ src/main/java/com/br/validadeproperties/ValidadePropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.br.validadeproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadePropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidadePropertiesApplication.class, args);
+    }
+
+}
